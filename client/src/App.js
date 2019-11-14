ac380e664339be6697535de231f21c51563165d3
rae-glazier <glazier.home.life@gmail.com>
2019-11-13 22:18:17 -0600

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,21 @@ import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 
+
 const App = () => {
 
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+  // const addToSavedList = movie => {
+  //   setSavedList( [...savedList, movie] );
+  // };
 
   return (
     <div>
       <SavedList list={savedList} />
-
-      <Route exact path ='/' component={MovieList} />
-      <Route path ='/Movie/:dataID' component={Movie} />
+      {/* <Movie /> */}
+      <Route exact path='/' component={MovieList} />
+      <Route exact path='/movies/:id' component={Movie}/>
 
     </div>
   );
